226831af3a0c13cf8e0570534346d8112df4aa1a
zahid1986 <newyorkbangla209@gmail.com>
2020-01-15 23:08:03 -0500

    Random

--- src/problemandsolutions/RandomNumberGenerate.java
+++ src/problemandsolutions/RandomNumberGenerate.java
@@ -1,7 +1,16 @@
 package problemandsolutions;
 
+import java.util.Random;
+
 public class RandomNumberGenerate {
     //Write Java program to generate a random number between 1 to 8.
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int lastNo =8;
+        int randomNumber = rd.nextInt(lastNo);
+        System.out.println("Random number is: " + (randomNumber+1));
+
+    }
 
 
 
