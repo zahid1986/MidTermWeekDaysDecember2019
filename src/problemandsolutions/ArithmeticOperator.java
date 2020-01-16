e4aa9306a870b2e846923f317bde19108f802235
zahid1986 <newyorkbangla209@gmail.com>
2020-01-15 22:36:25 -0500

    problemandsolutions

--- src/problemandsolutions/ArithmeticOperator.java
+++ src/problemandsolutions/ArithmeticOperator.java
@@ -1,11 +1,28 @@
 package problemandsolutions;
 
+import java.util.Scanner;
+
 public class ArithmeticOperator {
     //Write Java program to allow the user to input two integer values and then the program prints the results of adding,
     // subtracting, multiplying, and dividing among the two values.
 
 
     public static void main(String[] args) {
+
+        double a;
+        double b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first no: ");
+        a= sc.nextDouble();
+        System.out.println("Enter the second no: ");
+        b =sc.nextDouble();
+        System.out.println("Sum of two no is: " +(a+b));
+        System.out.println("subtracting of two no is: " + (a-b));
+        System.out.println("multiplying of two no is: "+ (a*b));
+        System.out.println("Divison is: " + (a/b));
+
+
+
 
     }
 
